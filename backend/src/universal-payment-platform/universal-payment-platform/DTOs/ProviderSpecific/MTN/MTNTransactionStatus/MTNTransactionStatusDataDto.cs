88114b53a6a8e851daaacb1194cad2c1402ca730
Clifford Hepplethwaite <cliffordrh@gmail.com>
2025-11-08 17:21:06 +0200

    Added MTN Transaction Status DTOs

---
+++ backend/src/universal-payment-platform/universal-payment-platform/DTOs/ProviderSpecific/MTN/MTNTransactionStatus/MTNTransactionStatusDataDto.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UniversalPaymentPlatform.DTOs.ProviderSpecific.MTN
+{
+    public record MTNTransactionStatusDataDto
+    {
+        [JsonPropertyName("amount")]
+        public decimal? Amount { get; init; }
+
+        [JsonPropertyName("paymentType")]
+        public string? PaymentType { get; init; }
+
+        [JsonPropertyName("customerId")]
+        public string? CustomerId { get; init; }
+
+        [JsonPropertyName("status")]
+        public string? Status { get; init; } // e.g., Success, Pending, Failed
+
+        [JsonPropertyName("description")]
+        public string? Description { get; init; }
+
+        [JsonPropertyName("targetSystem")]
+        public string? TargetSystem { get; init; }
+    }
+}
---
+++ backend/src/universal-payment-platform/universal-payment-platform/DTOs/ProviderSpecific/MTN/MTNTransactionStatus/MTNTransactionStatusRequestDto.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UniversalPaymentPlatform.DTOs.ProviderSpecific.MTN
+{
+    public record MTNTransactionStatusRequestDto
+    {
+        // Path Parameter
+        [JsonIgnore]
+        public required string CorrelatorId { get; init; }
+
+        // Query Parameters
+        [JsonPropertyName("amount")]
+        public decimal? Amount { get; init; }
+
+        [JsonPropertyName("customerId")]
+        public string? CustomerId { get; init; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; init; }
+
+        [JsonPropertyName("paymentType")]
+        public string? PaymentType { get; init; } // e.g., Airtime
+
+        [JsonPropertyName("targetSystem")]
+        public string? TargetSystem { get; init; } // e.g., EWP, ECW, CELD
+
+        // Headers (optional properties can be set in HTTP request)
+        [JsonIgnore]
+        public string? XAuthorization { get; init; }
+
+        [JsonIgnore]
+        public string? TransactionId { get; init; }
+    }
+}
