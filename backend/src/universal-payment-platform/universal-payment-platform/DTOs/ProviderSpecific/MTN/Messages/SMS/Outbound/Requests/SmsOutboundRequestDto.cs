270689fd48725af5f461c2324877ccb2e66f95e6
Clifford Hepplethwaite <cliffordrh@gmail.com>
2025-11-11 23:25:13 +0200

    Added sms outbound DTOs

---
+++ backend/src/universal-payment-platform/universal-payment-platform/DTOs/ProviderSpecific/MTN/Messages/SMS/Outbound/Requests/SmsOutboundRequestDto.cs
@@ -0,0 +1,7 @@
+﻿public class SmsOutboundRequestDto
+{
+    public string SenderAddress { get; set; }          // MSISDN or virtual MSISDN of sender
+    public List<string> ReceiverAddress { get; set; }  // Array of recipient MSISDN(s)
+    public string Message { get; set; }                // SMS message (max 160 characters)
+    public string ClientCorrelator { get; set; }      // Optional unique request identifier
+}
---
+++ backend/src/universal-payment-platform/universal-payment-platform/DTOs/ProviderSpecific/MTN/Messages/SMS/Outbound/Responses/SmsOutboundDataDto.cs
@@ -0,0 +1,4 @@
+﻿public class SmsOutboundDataDto
+{
+    public string Status { get; set; }               // Status of the submitted message, e.g., PENDING
+}
