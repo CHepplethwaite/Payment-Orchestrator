ca5fc1f9762299ee2c8dd0076e359f085a249b2f
Clifford Hepplethwaite <cliffordrh@gmail.com>
2025-11-14 20:39:12 +0200

    Added scafolfing for state management

---
+++ backend/src/universal-payment-platform/universal-payment-platform/StateMachine/Core/StateMachine.cs
@@ -0,0 +1,6 @@
+﻿namespace universal_payment_platform.StateMachine.Core
+{
+    public class StateMachine
+    {
+    }
+}
---
+++ backend/src/universal-payment-platform/universal-payment-platform/StateMachine/Events/PaymentProcessedEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace universal_payment_platform.StateMachine.Events
+{
+    public class PaymentProcessedEvent
+    {
+    }
+}
